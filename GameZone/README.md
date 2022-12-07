483f1eb3fe3fc5b2c23cd314e94e9c60c61a5f2a
Soumyajain644 <76671803+Soumyajain644@users.noreply.github.com>
2022-12-07 22:45:37 +0530

    Add files via upload

---
+++ GameZone/README.md
@@ -0,0 +1,20 @@
+# Browser-Game
+
+   Online Gaming is becoming more prevalant as the number of online users are increasing rapidly.
+   In addition to being fun, playing video games can reduce stress, lighten depression, increase vision, improve the ability to multi-task and improve decision-making skills.
+   But due to recent MMORPGs and other games that involves brutal and extreme graphics,peoples are forgetting the peaceful and strategy based games like chess,tic tac toe,...etc that were played among friends and family with enthusiasm.
+The main goal of our project is to revive the legacy video games that are forgotten in this ever growing world.
+
+# Approach
+We propose to build a website that will host free legacy games and players can play with friends(online multiplayer) or play with computer,just like the old days.
+
+# Progress
+1.First of all, player will be asked to login or Sign Up(if they haven't logged in before) so that their name can be registed in the Leaderboard
+![SignIn page](/screenshot/SignIn.jpg)
+
+![Signup page](/screenshot/signup.jpg)
+2.Now the player will be redirected to the home page of the website where he can play any one of the available games at a time.
+
+![Website page](/screenshot/website1.jpg)
+
+3.Now the player can chose to play any game and its score will be saved in the database and will be displayed on the leaderboard.
---
+++ GameZone/tic tac/Singleplayer/Simple/script.js
@@ -0,0 +1,95 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+  circleTurn = true
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
+
+function handleClick(e) {
+  const cell = e.target
+  placeMark(cell,X_CLASS)
+  circleTurn=!circleTurn
+  if (checkWin(X_CLASS)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "You Lose" : "You Win"}`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  placeMark(bestSpot(),CIRCLE_CLASS)
+  circleTurn=!circleTurn
+  if (checkWin(CIRCLE_CLASS)) {
+	endGame(false)
+  }
+}
+
+function bestSpot() {
+  let i
+  for(i=0;i<9;i++)
+  if(!(cellElements[i].classList.contains(X_CLASS)||cellElements[i].classList.contains(CIRCLE_CLASS)))
+  return cellElements[i]
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+    board.classList.add(X_CLASS)
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
